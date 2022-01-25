085517646c3d7276bef6fd3b78ccaa2f09692931
Luma Montes <lumagoesmontes@gmail.com>
2022-01-25 19:26:49 -0300

    adicionado rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import DashboardAdm from './pages/DashboardAdm';
 
 function App() {
-  return <p>oiiiiiiiiiii</p>;
+  return (
+    <BrowserRouter>
+      {/* <AuthContextProvider> */}
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/dashboard_adm" component={DashboardAdm} />
+        </Switch>
+      {/* </AuthContextProvider> */}
+    </BrowserRouter>
+  );
 }
 
 export default App;
