72dc1c2f141f4f553bbef0c761bc34c91b5b968d
Luma Montes <lumagoesmontes@gmail.com>
2022-01-25 20:42:31 -0300

    criado componente de card de curso e iniciado estilizacao de dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import CursoCard from '../../components/CursoCard';
+import './styles.css';
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className='container'>
+        <header className='sidebar'>
+            <img src="https://imgur.com/nk2Pg72.jpg" alt="Proesc Academy"/>
+            <Link to="/">Sair</Link>
+        </header>
+
+        <div className="tabsContainer">
+            <div className='cursosButtons'>
+                <button className='cursosButton'>Cursos em andamento</button>
+                <button className='cursosButton hidden'>Cursos finalizados</button>
+            </div>
+            <div className="cursosCards">
+                <CursoCard />
+                <CursoCard />
+                <CursoCard />
+                <CursoCard />
+            </div>
+        </div>
+
+    </div>
+    
   );
 }
 
