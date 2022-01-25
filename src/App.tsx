08fca023658062e87dea77e510a3cf5bec54f19a
Lorena <lorena@proesc.com>
2022-01-25 19:36:06 -0300

    Ajustada rota

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
@@ -7,15 +7,15 @@ import DashboardAdm from './pages/DashboardAdm';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       {/* <AuthContextProvider> */}
-        <Switch>
-          <Route path="/" exact component={Login} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/dashboard_adm" component={DashboardAdm} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard_adm" element={<DashboardAdm />} />
+        </Routes>
       {/* </AuthContextProvider> */}
-    </BrowserRouter>
+    </Router>
   );
 }
 
