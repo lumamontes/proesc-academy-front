4299557943b83360650d5fa2e7a4d1a80e4f35d0
Lucas Edilson <lucas@proesc.com>
2022-02-01 16:09:05 -0300

    Finalizada tela de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import './styles.css';
+import './LoginPhoto.jpg';
 
 function Login() {
   return (
     <>
-      <main className="conteudo">
-        <div className="titulo-proesc">Proesc</div>
-        <div className="subtitulo-academy">Academy</div>
-      </main>
-      <form className="formulario conteudo">
-        <input type="email" name="name" placeholder='Email' />
-        <input type="password" name="password" placeholder='Senha' />
-        <button className='button-login' type="submit">Login com o Proesc</button>
-      </form>
+      <div className='Logo'></div>    
+      <div className='formulario_responsivo'>
+        <main className="conteudo">
+          <div className="titulo-proesc">Proesc</div>
+          <div className="subtitulo-academy">Academy</div>
+        </main>
+        <form className="formulario conteudo">
+          <input type="email" name="name" placeholder='Email' />
+          <input type="password" name="password" placeholder='Senha' />
+          <button className='button-login' type="submit">Login com o Proesc</button>
+        </form>
+      </div>
     </>
   );
 }
